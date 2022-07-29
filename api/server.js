9d573b25818c39e03bf3b9f650970a2ac1975e05
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-07-29 16:28:39 +1000

    fixed the server route

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 const dbURL = "mongodb://127.0.0.1:27017/grouprepo";
 
-app.use("/", (res) => res.send("Hello World"));
+app.use("/", (req, res) => res.send("Hello World"));
 
 app.listen(PORT, () => {
   console.log("Listening on", PORT);
