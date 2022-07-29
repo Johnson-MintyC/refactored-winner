d6b56b936a352e5454b52f701b04c32e802e21eb
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-07-29 15:43:35 +1000

    mongo removed

--- api/server.js
+++ api/server.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3000;
 const dbURL = "mongodb://127.0.0.1:27017/grouprepo";
 
-app.use("/", res.send("Hello World"));
-
-mongoose.connect(dbURL, () => {
-  console.log("Connected to doggos db");
-});
+app.use("/", (res) => res.send("Hello World"));
 
 app.listen(PORT, () => {
   console.log("Listening on", PORT);
